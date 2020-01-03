d4a9aaa6ea3454b8ceb4c9bdd4426637261665a2
Chris Cook <christopher.cook@outlook.com.au>
2020-01-04 04:46:54 +1100

    Add more util functions

--- src/Util.ts
+++ src/Util.ts
@@ -1,3 +1,22 @@
 export function lerp(a: number, b: number, mix: number): number {
   return (a * (1 - mix)) + (b * mix);
 }
+
+export function mapValue(
+  value: number,
+  inFrom: number,
+  inTo: number,
+  outFrom: number,
+  outTo: number,
+): number {
+  const inRange = inTo - inFrom;
+  const outRange = outTo - outFrom;
+  const progress = (value - inFrom) / inRange;
+  return (progress * outRange) + outFrom;
+}
+
+export function nextFrame() {
+  return new Promise((resolve) => {
+    requestAnimationFrame(() => resolve());
+  });
+}
