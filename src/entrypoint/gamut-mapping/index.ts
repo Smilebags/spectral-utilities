e588da344dd1f258e7f47be988fe16243f2acc7e
Chris Cook <christopher.cook@outlook.com.au>
2020-05-30 16:50:38 +1000

    Cleanup

--- src/entrypoint/gamut-mapping/index.ts
+++ src/entrypoint/gamut-mapping/index.ts
@@ -1,10 +1,7 @@
 import Colour from "../../Colour.js";
-import { mapValue, logN } from "../../Util.js";
+import { mapValue } from "../../Util.js";
 import CanvasOutput from "../../CanvasOutput.js";
 import { Vec2, Vec3 } from "../../Vec.js";
-import { ParabolicSpectrum as InverseSquareSpectrum } from "../../Spectrum/ParabolicSpectrum.js";
-import { WrappingParabolicSpectrum } from "../../Spectrum/WrappingParabolicSpectrum.js";
-import { Spectrum } from "../../types/index.js";
 import { GaussianSpectrum } from "../../Spectrum/GaussianSpectrum.js";
 
 const WAVELENGTH_LOW = 360;
@@ -22,15 +19,12 @@ const locusEl = document.querySelector('#locusWavelength') as HTMLInputElement;
 const pinkProgressEl = document.querySelector('#pinkProgress') as HTMLInputElement;
 const canvasEl = document.querySelector('canvas')!;
 const canvasOutput = new CanvasOutput(canvasEl, CANVAS_SIZE, CANVAS_SIZE, false, 2.2, 0.18);
-canvasOutput.clear(true);
 
 
 const currentState = {
   locusWavelength: 550,
   currentPinkProgress: 50,
 };
-// const partFunction = (x: number) => (((((x**2) - (2 * x)) ** 0.5) - x + 1) ** (1/3));
-// const inverseSmoothStep = (x: number) => partFunction(x) + 1 / (partFunction(x)) + 1;
 
 const locusSamples = new Array(LOCUS_SAMPLES)
   .fill(null)
@@ -51,32 +45,22 @@ const boundarySamples = [
   ...pinkEdgeSamples,
 ];
 
-drawPoints(boundarySamples);
 
-
-// function drawDot(point: Colour): void {
-//   const xyYlocation = point.toxyY();
-//   const location = new Vec2(xyYlocation.triplet.x, xyYlocation.triplet.y).add(0.1);
-//   canvasOutput.drawCircle({
-//     radius: 0.05,
-//     location,
-//     color: point,
-//   });
-// }
+function drawDot(point: Colour): void {
+  const xyYlocation = point.toxyY();
+  const location = new Vec2(xyYlocation.triplet.x, xyYlocation.triplet.y).add(0.1);
+  canvasOutput.drawCircle({
+    radius: 0.05,
+    location,
+    color: point,
+  });
+}
 
 function drawPoints(points: Colour[]): void {
   points
     .map(point => {
       const xyYlocation = point.toxyY();
       const mappedLocation = new Vec2(xyYlocation.triplet.x, xyYlocation.triplet.y).add(0.1);
-        // .add(new Vec2(0.2, 0.05))
-        // .multiply(new Vec2(1, -1))
-        // .add(new Vec2(0, 1))
-        // .multiply(CANVAS_SIZE);
-      // canvasOutput.setPixel(
-      //   point.toRec709().triplet.normalise(),
-      //   new Vec2(Math.round(mappedLocation.x), Math.round(mappedLocation.y)),
-      // );
         return {
           location: mappedLocation,
           colour: point,
@@ -86,13 +70,8 @@ function drawPoints(points: Colour[]): void {
       if (index === 0) {
         return;
       }
-      // canvasOutput.drawCircle({
-      //   radius: 0.003,
-      //   location,
-      //   color: colour,
-      // });
       canvasOutput.drawLine({
-        lineWidth: 0.005,
+        lineWidth: 0.003,
         from: arr[index - 1].location,
         to: location,
         color: colour,
@@ -100,37 +79,36 @@ function drawPoints(points: Colour[]): void {
   });
 }
 
-const wavelengthRange = WAVELENGTH_HIGH - WAVELENGTH_LOW;
-new Array(25)
-  .fill(null)
-  .forEach(
-    (item, index) => {
-      currentState.locusWavelength = mapValue(index, 0, 24, WAVELENGTH_LOW, WAVELENGTH_HIGH);
-      currentState.currentPinkProgress = mapValue(index, 0, 24, 0, 100);
-      renderDesaturationCurve();
-    });
+// new Array(25)
+//   .fill(null)
+//   .forEach(
+//     (item, index) => {
+//       currentState.locusWavelength = mapValue(index, 0, 24, WAVELENGTH_LOW, WAVELENGTH_HIGH);
+//       currentState.currentPinkProgress = mapValue(index, 0, 24, 0, 100);
+//       renderDesaturationCurve();
+//     });
 
 
-// locusEl.addEventListener('input', (e) => {
-//   if (!e) {
-//     return;
-//   }
-//   currentState.locusWavelength = Number((event!.target as HTMLInputElement).value);
-//   renderDesaturationCurve();
-// });
-// pinkProgressEl.addEventListener('input', (e) => {
-//   if (!e) {
-//     return;
-//   }
-//   currentState.currentPinkProgress = Number((event!.target as HTMLInputElement).value);
-//   renderDesaturationCurve();
-// });
+locusEl.addEventListener('input', (e) => {
+  if (!e) {
+    return;
+  }
+  currentState.locusWavelength = Number((event!.target as HTMLInputElement).value);
+  render();
+});
+pinkProgressEl.addEventListener('input', (e) => {
+  if (!e) {
+    return;
+  }
+  currentState.currentPinkProgress = Number((event!.target as HTMLInputElement).value);
+  render();
+});
 
-// renderDesaturationCurve();
+render();
 
 type DesaturationStrategy = (wavelength: number, amount: number) => Colour;
 
-function renderDesaturationCurve() {
+function render() {
   const locusWavelength = currentState.locusWavelength;
   console.log(locusWavelength);
   const pinkProgress = currentState.currentPinkProgress;
@@ -146,7 +124,6 @@ function renderDesaturationCurve() {
       WAVELENGTH_LOW,
       width * progress,
     ), 2 ** 8);
-    // const blueColor = Colour.fromWavelength(WAVELENGTH_LOW);
     const blueEnergy = blueColor.sum;
 
 
@@ -154,21 +131,12 @@ function renderDesaturationCurve() {
       WAVELENGTH_HIGH,
       width * progress,
     ), 2 ** 8);
-    // const redColor = Colour.fromWavelength(WAVELENGTH_HIGH);
 
     const redEnergy = redColor.sum;
 
     const scaledBlue = blueColor.multiply((blueEnergy + redEnergy) / blueEnergy);
     const scaledRed = redColor.multiply((blueEnergy + redEnergy) / redEnergy);
-    // console.log(scaledBlue.lerp(scaledRed, progress / 100).triplet);
     return scaledBlue.lerp(scaledRed, progress / 100);
-
-    // const attemptOne = lowColor.lerp(highColor, logN(1-(progress / 100), 3));
-    // const attemptOne = lowColor.lerp(highColor, 1-(progress / 100));
-    // const lum = attemptOne.toxyY().triplet.z;
-    // return attemptOne.lerp(new Colour(new Vec3(lum, lum, lum),'XYZ'), desaturationAmount);
-
-    // return lowColor.add(highColor.multiply(progress / 100));
   };
 
   const LobeWideningStrategy: DesaturationStrategy = (wavelength, amount) => {
@@ -177,30 +145,12 @@ function renderDesaturationCurve() {
       wavelength,
       width,
     );
-    return Colour.fromSpectrum(spectrum, 2 ** 11);
+    return Colour.fromSpectrum(spectrum, 2 ** 8);
   };
 
   const XYZLerpStrategy: DesaturationStrategy = (wavelength, amount) => {
     return Colour.fromWavelength(wavelength).lerp(new Colour(new Vec3(1, 1, 1)), amount ** 2);
   };
-
-  // const OpposingLobeMixStrategy: DesaturationStrategy = (wavelength, amount) => {
-  //   const width = mapValue(amount ** 3, 0, 1, 0.1, 300);
-  //   const spectrum = new WrappingParabolicSpectrum(
-  //     wavelength,
-  //     width,
-  //   );
-  //   return Colour.fromSpectrum(spectrum, 2** 8);
-  // };
-  // const OpposingColourMixStrategy: DesaturationStrategy = (wavelength, amount) => {
-  //   const width = mapValue(amount ** 3, 0, 1, 0.1, 300);
-  //   const spectrum = new ParabolicSpectrum(
-  //     wavelength,
-  //     width,
-  //   );
-
-  //   return Colour.fromSpectrum(spectrum, 2 ** 8);
-  // };
 
   const lobeDesaturationSamples = new Array(DESATURATION_SAMPLES)
     .fill(null)
@@ -215,37 +165,8 @@ function renderDesaturationCurve() {
       return PinkLobeWideningStrategy(pinkProgress, desaturationAmount);
     });
 
-  // const lerpDesaturationSamples = new Array(DESATURATION_SAMPLES)
-  //   .fill(null)
-  //   .map((item, index) => {
-  //     const desaturationAmount = mapValue(index, 0, DESATURATION_SAMPLES - 1, 0, 1)
-  //     return XYZLerpStrategy(locusWavelength, desaturationAmount);
-  //   });
-    
-  // const mixedDesaturationSamples = new Array(DESATURATION_SAMPLES)
-  //   .fill(null)
-  //   .map((item, index) => {
-  //     const desaturationAmount = mapValue(index, 0, DESATURATION_SAMPLES - 1, 0, 1)
-  //     const xyz = XYZLerpStrategy(locusWavelength, desaturationAmount);
-  //     const lobe = LobeWideningStrategy(locusWavelength, desaturationAmount);
-  //     return xyz.lerp(lobe, 0.9);
-  //   });
-
-  // const opposingLobeMixSamples = new Array(DESATURATION_SAMPLES)
-  // .fill(null)
-  // .map((item, index) => {
-  //   const desaturationAmount = mapValue(index, 0, DESATURATION_SAMPLES - 1, 0, 1)
-  //   const xyz = XYZLerpStrategy(wavelength, desaturationAmount);
-  //   const lobe = OpposingLobeMixStrategy(wavelength, desaturationAmount);
-  //   return xyz.lerp(lobe, 0.9);
-  // });
-
+  canvasOutput.clear(true);
   drawPoints(boundarySamples);
-
   drawPoints(pinkDesaturationSamples);
-
   drawPoints(lobeDesaturationSamples);
-
-  // drawPoints(lerpDesaturationSamples);
-  // drawPoints(mixedDesaturationSamples);
 }
