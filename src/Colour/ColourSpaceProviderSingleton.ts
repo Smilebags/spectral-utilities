846848d5fafe5c1beae6583b3bc7cf5ef96c93e1
Chris Cook <christopher.cook@outlook.com.au>
2020-10-01 21:15:02 +1000

    Update chromatic adaptation
    Update the matrix to the one provided by python's Colour Science and
    remove the adaptation step from all but one colour space to allow
    explicit handling of white point adaptation.

--- src/Colour/ColourSpaceProviderSingleton.ts
+++ src/Colour/ColourSpaceProviderSingleton.ts
@@ -6,9 +6,9 @@ import { Vec3 } from "../Vec.js";
 
 function toSrgbEotf(colour: Vec3) {
   return new Vec3(
-    colour.x < 0 ? -1 : colour.x ** (1 / 2.2),
-    colour.y < 0 ? -1 : colour.y ** (1 / 2.2),
-    colour.z < 0 ? -1 : colour.z ** (1 / 2.2),
+    colour.x < 0 ? 0 : colour.x ** (1 / 2.2),
+    colour.y < 0 ? 0 : colour.y ** (1 / 2.2),
+    colour.z < 0 ? 0 : colour.z ** (1 / 2.2),
   );
 }
 function fromSrgbEotf(colour: Vec3) {
@@ -20,17 +20,16 @@ function fromSrgbEotf(colour: Vec3) {
 }
 
 const toD65: Matrix = [
-  [0.9531874, -0.0265906, 0.0238731],
-  [-0.0382467, 1.0288406, 0.0094060],
-  [0.0026068, -0.0030332, 1.0892565],
+  [0.95314749, -0.02661093, 0.02391814],
+  [-0.03827288, 1.02885155, 0.00942132],
+  [0.00261508, -0.0030468, 1.08948201],
 ];
 
 const fromD65: Matrix = [
-  [1.0502616, 0.0270757, -0.0232523],
-  [0.0390650, 0.9729502, -0.0092579],
-  [-0.0024047, 0.0026446, 0.9180873],
+  [1.05030753, 0.0270969, -0.02329244],
+  [0.03909312, 0.97294119, -0.00927177],
+  [-0.00241173, 0.00265585, 0.91789736],
 ];
-
 
 const rec709 = new GenericColourSpace(
   'REC.709',
@@ -44,8 +43,6 @@ const rec709 = new GenericColourSpace(
     [0.2126729, 0.7151522, 0.0721750,],
     [0.0193339, 0.1191920, 0.9503041,],
   ],
-  toD65,
-  fromD65,
 );
 
 const rec2020 = new GenericColourSpace(
@@ -60,8 +57,6 @@ const rec2020 = new GenericColourSpace(
     [2.62698339e-01, 6.78008766e-01, 5.92928953e-02],
     [4.99407097e-17, 2.80731358e-02, 1.06082723e+00],
   ],
-  toD65,
-  fromD65,
 );
 
 const dcip3 = new GenericColourSpace(
@@ -76,8 +71,6 @@ const dcip3 = new GenericColourSpace(
     [2.09491678e-01, 7.21595254e-01, 6.89130679e-02],
     [-3.63410132e-17, 4.70605601e-02, 9.07355394e-01],
   ],
-  toD65,
-  fromD65,
 );
 
 const displayP3: ColourSpace = {
@@ -138,7 +131,7 @@ const XYZD65 = new GenericColourSpace(
   ],
   [
     [1,0,0],
-    [0,1,0,],
+    [0,1,0],
     [0,0,1],
   ],
   toD65,
