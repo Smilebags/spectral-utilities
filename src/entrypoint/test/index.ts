c52a80465589e093954189bad921c07da4e5bae1
Chris Cook <christopher.cook@outlook.com.au>
2020-10-08 01:05:02 +1100

    Add tests

--- src/entrypoint/test/index.ts
+++ src/entrypoint/test/index.ts
@@ -4,7 +4,7 @@ import { Vec3 } from "../../Vec.js";
 
 import { test, expect } from './test.js';
 
-test('test tolerance works', () => {
+test('test tolerance', () => {
   expect(10).toBeCloseTo(10);
   expect(() => expect(10).toBeCloseTo(11)).toThrow();
   expect(10).toBeCloseTo(10.0001, 4);
@@ -13,7 +13,7 @@ test('test tolerance works', () => {
   expect(10).toBeCloseTo(100, -2);
 });
 
-test(`White XYZ to xyY works`, () => {
+test(`White XYZ to xyY`, () => {
   const base = new Colour(new Vec3(1, 1, 1), 'XYZ');
   const destination = base.to('xyY');
   expect(destination.triplet.x).toBeCloseTo(1/3);
@@ -21,7 +21,7 @@ test(`White XYZ to xyY works`, () => {
   expect(destination.triplet.z).toBeCloseTo(1);
 });
 
-test(`White XYZ to XYZD65 works`, () => {
+test(`White XYZ to XYZD65`, () => {
   const base = new Colour(new Vec3(1, 1, 1), 'XYZ');
   const destination = base.to('XYZD65');
   expect(destination.triplet.x).toBeCloseTo(0.9504547, 6);
@@ -29,7 +29,7 @@ test(`White XYZ to XYZD65 works`, () => {
   expect(destination.triplet.z).toBeCloseTo(1.08905, 6);
 });
 
-test(`XYZD65 round trip works`, () => {
+test(`XYZD65 round trip`, () => {
   const base = new Colour(new Vec3(1, 1, 1), 'XYZ');
   const destination = base.to('XYZD65').to('XYZ');
   expect(destination.triplet.x).toBeCloseTo(1);
@@ -37,7 +37,7 @@ test(`XYZD65 round trip works`, () => {
   expect(destination.triplet.z).toBeCloseTo(1);
 });
 
-test(`White XYZ to REC.709 works`, () => {
+test(`White XYZ to REC.709`, () => {
   const base = new Colour(new Vec3(1, 1, 1), 'XYZ');
   const adapted = base.to('XYZD65');
   adapted.colourSpace = 'XYZ';
@@ -55,7 +55,7 @@ const rec709ToxyYTestCases: [string, Vec3, Vec3][] = [
   ['Blue', new Vec3(0, 0, 1), new Vec3(0.150, 0.060, 0.0722)],
 ];
 rec709ToxyYTestCases.forEach(([colourName, sourceTriplet, expectedXY]) => {
-  test(`${colourName} REC.709 to xyY works`, () => {
+  test(`${colourName} REC.709 to xyY`, () => {
     const base = new Colour(sourceTriplet, 'REC.709');
     const destination = base.to('xyY');
     expect(destination.triplet.x).toBeCloseTo(expectedXY.x, 3);
@@ -74,7 +74,7 @@ const rec709TosRGBTestCases: [string, Vec3, Vec3][] = [
   ['Blue', new Vec3(0, 0, 1), new Vec3(0, 0, 1)],
 ];
 rec709TosRGBTestCases.forEach(([colourName, sourceTriplet, expectedTriplet]) => {
-  test(`${colourName} REC.709 to sRGB works`, () => {
+  test(`${colourName} REC.709 to sRGB`, () => {
     const base = new Colour(sourceTriplet, 'REC.709');
     const destination = base.to('sRGB');
     expect(destination.triplet.x).toBeCloseTo(expectedTriplet.x, 3);
@@ -95,7 +95,7 @@ const colourSpaces: ColourSpaceName[] = [
 ];
 
 colourSpaces.forEach((colourSpace) => {
-  test(`Colour round-trip to ${colourSpace} works`, () => {
+  test(`Colour round-trip to ${colourSpace}`, () => {
     const base = new Colour(new Vec3(0.35,0.3, 0.4), 'XYZ');
     const roundTrip = base.to(colourSpace).to('XYZ')
     expect(roundTrip.triplet.x).toBeCloseTo(0.35, 7);
@@ -104,10 +104,32 @@ colourSpaces.forEach((colourSpace) => {
   });
 });
 
-test(`Colour round-trip from REC.709 to sRGB works`, () => {
+test(`Colour round-trip from REC.709 to sRGB`, () => {
   const base = new Colour(new Vec3(0.35,0.3, 0.4), 'REC.709');
   const roundTrip = base.to('sRGB').to('REC.709')
   expect(roundTrip.triplet.x).toBeCloseTo(0.35, 6);
   expect(roundTrip.triplet.y).toBeCloseTo(0.3, 6);
   expect(roundTrip.triplet.z).toBeCloseTo(0.4, 6);
+});
+
+
+
+test(`Colour hex`, () => {
+  const a = new Colour(new Vec3(0,0.5, 1), 'sRGB');
+  const b = new Colour(new Vec3(0.2,0.5, 1), 'sRGB');
+  const c = new Colour(new Vec3(1, 1, 1), 'sRGB');
+  const d = a.multiply(0.5);
+  expect(a.hex).toBe('#0080ff');
+  expect(b.hex).toBe('#3380ff');
+  expect(c.hex).toBe('#ffffff');
+  expect(d.hex).toBe('#004080');
+});
+
+test(`Colour mixing in sRGB`, () => {
+  const a = new Colour(new Vec3(0.2,0.5, 1), 'sRGB');
+  const b = new Colour(new Vec3(1, 1, 1), 'sRGB');
+  const result = a.lerp(b, 0.5);
+  expect(result.triplet.x).toBeCloseTo(0.6, 6);
+  expect(result.triplet.y).toBeCloseTo(0.75, 6);
+  expect(result.triplet.z).toBeCloseTo(1, 6);
 });
